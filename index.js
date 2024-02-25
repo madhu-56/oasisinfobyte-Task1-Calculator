a9b0ae6819508e5a182e6cf2aad15c1eaf269e21
MadhuKumari <madhu03102001@gmail.com>
2024-02-25 10:17:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var result = document.getElementById("result");
+
+function display(number){
+    result.value += number;
+}
+function calculator(){
+    var final_number = result.value;
+    var final_result = eval(final_number);
+    result.value = final_result;
+
+}
+
+function clrs(){
+    result.value="";
+}
+
+function del(){
+    result.value = result.value.slice(0,-1);
+}
